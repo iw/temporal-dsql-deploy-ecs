3a477890dbaa40d880942e6f52bf6915dac35ad5
Ian Wilkinson <ianw1@mac.com>
2026-01-20 11:37:22 +0000

    feat: add DynamoDB gateway VPC endpoint for distributed rate limiter

--- terraform/vpc-endpoints.tf
+++ terraform/vpc-endpoints.tf
@@ -170,3 +170,15 @@ resource "aws_vpc_endpoint" "s3" {
     Name = "${var.project_name}-s3-endpoint"
   }
 }
+
+# DynamoDB Gateway endpoint (com.amazonaws.region.dynamodb) - for DSQL distributed rate limiter
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.dynamodb"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+
+  tags = {
+    Name = "${var.project_name}-dynamodb-endpoint"
+  }
+}
